995fbf59f9607a7c231ac5a549e4d7862f1d5612
Thomas JARRY <12656492+snow3461@users.noreply.github.com>
2019-05-06 12:17:33 +0200

    Adjusted git ignore file

---
+++ Image_Analysis.m
@@ -0,0 +1,22 @@
+clear all;
+close all;
+
+addpath('functions');%add the functions directory to the path for this session only.
+fullpaths=get_paths('D:\Thèse_INSA\Experiences\');
+
+timetbl=create_time_points(length(fullpaths));
+
+% tablearray=singlefile_treatment(fullpaths);
+tablearray=parallel_treatment(fullpaths);%treat_images
+
+tablearray=populate_timepoint(tablearray,timetbl);%add time correspondance
+
+%% attribute to each bubbles
+data=identifybubble(tablearray);
+
+%% apply scaling
+data=apply_scaling(data);
+
+%%save file
+
+save_results( fullpaths,data );
